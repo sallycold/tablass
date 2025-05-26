0c2a1c262a7dff8ecfa3e16d039568f86f68a9d4
Toso <tosobrisa4@gmail.com>
2025-05-26 17:00:21 -0300

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,214 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.campos = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(295, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(149, 13)
+        Me.Label2.TabIndex = 19
+        Me.Label2.Text = "VER REGIDTRO EN EL DTG"
+        '
+        'campos
+        '
+        Me.campos.AutoSize = True
+        Me.campos.Location = New System.Drawing.Point(-54, -4)
+        Me.campos.Name = "campos"
+        Me.campos.Size = New System.Drawing.Size(44, 13)
+        Me.campos.TabIndex = 18
+        Me.campos.Text = "campos"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(39, 262)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(326, 119)
+        Me.DataGridView1.TabIndex = 17
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(190, 175)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 35)
+        Me.Button5.TabIndex = 16
+        Me.Button5.Text = "VER / CAMPO"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(190, 134)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 35)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "SACAR <"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(190, 92)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 36)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "AGREGAR >"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(312, 92)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(120, 147)
+        Me.ListBox2.TabIndex = 11
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(54, 92)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(106, 147)
+        Me.ListBox1.TabIndex = 10
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(51, 76)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 13)
+        Me.Label1.TabIndex = 20
+        Me.Label1.Text = "CAMPOS"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(54, 38)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 21
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(51, 22)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(100, 13)
+        Me.Label3.TabIndex = 22
+        Me.Label3.Text = "TABLAS DE LA BD"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(384, 345)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 15
+        Me.Button4.Text = "SALIR"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(359, 38)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 23
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"que comience", "que termine", "que contenga"})
+        Me.ComboBox2.Location = New System.Drawing.Point(228, 38)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(107, 21)
+        Me.ComboBox2.TabIndex = 24
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(225, 22)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(93, 13)
+        Me.Label4.TabIndex = 25
+        Me.Label4.Text = "Prametro a buscar"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(492, 393)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.campos)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents campos As System.Windows.Forms.Label
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,164 @@
+﻿Imports System.Data.OleDb
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Public Class Form1
+
+    Dim bbdd1 As OleDbConnection
+    Dim sConnString As OleDbConnection
+    Dim cm As OleDbCommand
+    Dim da As OleDbDataAdapter
+    Dim ds As DataSet
+    Dim dv As New DataView
+    Dim consulta As String
+    Dim CN As OleDbConnection = New OleDbConnection("provider=Microsoft.ace.oledb.12.0;data source=cajas.accdb;")
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.DataSource = ObtenerTablas("*")
+        DataGridView1.Visible = False
+
+    End Sub
+    Friend Function ObtenerTablas(ByVal nombreInicio As String) As String()
+        Dim dt As DataTable = Nothing
+        Using CN
+            CN.Open()
+            dt = CN.GetSchema("TABLES")
+        End Using
+        'Para SQL Server el valor de TABLE_TYPE es BASE TABLE.
+        Return (From row As DataRow In dt.Rows.Cast(Of DataRow)()
+                Where CStr(row("TABLE_TYPE")) = "TABLE" AndAlso CStr(row("TABLE_NAME")) Like nombreInicio
+                Order By row("TABLE_NAME")
+                Select CStr(row("TABLE_NAME"))).ToArray()
+    End Function
+    Private Sub cargar_bd()
+        ListBox1.Items.Clear()
+        CN = New OleDbConnection("provider=Microsoft.ace.oledb.12.0;data source = cajas.accdb;")
+        cm = New OleDbCommand("Select * FROM " & ComboBox1.Text & "", CN) ' muestra los campos de una BD en un combo
+        da = New OleDbDataAdapter(cm)
+
+        CN.Open()
+        ds = New DataSet()
+        da.Fill(ds)
+
+        Dim i As Integer
+        For i = 0 To ds.Tables(0).Columns.Count - 1
+            ListBox1.Items.Add(ds.Tables(0).Columns(i).ColumnName) ' campos de la tabla que seleccione
+        Next
+        DataGridView1.DataSource = ds.Tables(0)
+        CN.Close()
+
+    End Sub
+
+
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        DataGridView1.Visible = True
+
+        ' Validación: asegurarse de que se ha ingresado una palabra clave
+        If TextBox1.Text.Trim() = "" Then
+            MsgBox("Debe ingresar un valor a buscar", MsgBoxStyle.Critical, "Búsqueda de un valor")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+
+        ' Conexión a la base de datos
+        Dim CN As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=cajas.accdb;")
+        Dim tabla As String = ComboBox1.Text.Trim()
+
+        ' Construcción de los campos seleccionados
+        Dim camposSQL As String = ""
+        For i As Integer = 0 To ListBox2.Items.Count - 1
+            camposSQL &= "[" & ListBox2.Items(i).ToString() & "],"
+        Next
+        camposSQL = camposSQL.TrimEnd(","c) ' Elimina la última coma
+
+        ' Construcción del patrón LIKE según el tipo de búsqueda
+        Dim tipoBusqueda As String = ComboBox2.Text
+        Dim palabraClave As String = TextBox1.Text.Trim()
+        Dim operadorLike As String = ""
+
+        Select Case tipoBusqueda
+            Case "que comience"
+                operadorLike = palabraClave & "%"
+            Case "que termine"
+                operadorLike = "%" & palabraClave
+            Case "que contenga"
+                operadorLike = "%" & palabraClave & "%"
+            Case Else
+                operadorLike = "%" & palabraClave & "%"
+        End Select
+
+        ' Crear cláusula WHERE con varios LIKE unidos por OR
+        Dim whereClause As String = ""
+        For i As Integer = 0 To ListBox2.Items.Count - 1
+            whereClause &= "[" & ListBox2.Items(i).ToString() & "] LIKE ?"
+            If i < ListBox2.Items.Count - 1 Then
+                whereClause &= " OR "
+            End If
+        Next
+
+        ' Armar la consulta final
+        Dim consulta As String = "SELECT " & camposSQL & " FROM [" & tabla & "] WHERE " & whereClause
+
+        ' Crear comando y agregar parámetros
+        Dim comando As New OleDbCommand(consulta, CN)
+        For i As Integer = 0 To ListBox2.Items.Count - 1
+            comando.Parameters.AddWithValue("?", operadorLike)
+        Next
+
+        ' Ejecutar la consulta
+        Try
+            Dim adaptador As New OleDbDataAdapter(comando)
+            Dim resultado As New DataTable()
+
+            CN.Open()
+            adaptador.Fill(resultado)
+            DataGridView1.DataSource = resultado
+
+            If resultado.Rows.Count = 0 Then
+                MsgBox("No se encontraron registros.", MsgBoxStyle.Information, "Resultado")
+            End If
+        Catch ex As Exception
+            MsgBox("Error: " & ex.Message, MsgBoxStyle.Critical, "Error de búsqueda")
+        Finally
+            CN.Close()
+        End Try
+
+
+
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If ListBox1.SelectedItem <> "" Then
+            ListBox2.Items.Add(ListBox1.SelectedItem)
+            ListBox1.Items.Remove(ListBox1.SelectedItem)
+            ListBox1.ClearSelected()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If ListBox2.SelectedItem <> "" Then
+            ListBox1.Items.Add(ListBox2.SelectedItem)
+            ListBox2.Items.Remove(ListBox2.SelectedItem)
+            ListBox2.ClearSelected()
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+        ListBox1.Items.Clear()
+        ListBox2.Items.Clear()
+        DataGridView1.Visible = False
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        cargar_bd()
+        ComboBox1.Text = "Seleccione el campo a buscar"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        End
+
+    End Sub
+    Private Sub BUSQUEDA()
+       
+    End Sub
+End Class
